11f445992ff219f6215979ec71c2dc8323a3c821
Yuhuan Jiang <jyuhuan@gmail.com>
2017-03-01 18:06:13 -0500

    updated

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,17 @@
-console.log("Hello, from main.ts!")
+class Greeter {
+  name: string;
+  message: string;
+  
+  public constructor(name: string, msg: string) {
+    this.name = name;
+    this.message = msg;
+  }
+
+  public getGreetingMessage(): string {
+    return `[${this.name}] ${this.message}`
+  }
+}
+
+const greeter = new Greeter("main.ts", "Hello, from main.ts!")
+
+console.log(greeter.getGreetingMessage())
--- target/js/main.js
+++ target/js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello, from main.ts!");
+var Greeter = (function () {
+    function Greeter(name, msg) {
+        this.name = name;
+        this.message = msg;
+    }
+    Greeter.prototype.getGreetingMessage = function () {
+        return "[" + this.name + "] " + this.message;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("main.ts", "Hello, from main.ts!");
+console.log(greeter.getGreetingMessage());
